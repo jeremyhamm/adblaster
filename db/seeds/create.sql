389976282b219df9086aa2ec50a27cbf07b02a40
jh@jeremyhamm.dev <jh@jeremyhamm.dev>
2021-05-15 11:59:59 -0700

    Update adlist json and add description to domains column

--- db/seeds/create.sql
+++ db/seeds/create.sql
@@ -9,6 +9,7 @@ Create table for domains
 CREATE TABLE IF NOT EXISTS domains (
   id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
+  description TEXT NULL,
   ip VARCHAR(15) NULL,
   host VARCHAR(255) NOT NULL, 
   category VARCHAR(50) NOT NULL,
